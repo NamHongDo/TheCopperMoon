21de6e932dbaa510e2e70ae5996691340ff8aeee
Nam Do <sirsunday89@gmail.com>
2017-05-02 12:01:14 -0400

    faviicon added

--- getItems.php
+++ getItems.php
@@ -4,31 +4,10 @@
   <?php
   $q = intval($_GET['q']);
   $g=$q+30;
-  try {
-      $conn = new PDO("sqlsrv:server = tcp:coppermoon.database.windows.net,1433; Database = thecoppermoon", "namdo", "Qu3nm4tr0i");
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch (PDOException $e) {
-      print("Error connecting to SQL Server.");
-      die(print_r($e));
-  }
+  $con = mysqli_connect("localhost","root","","thecoppermoon") or die("Error".mysqli_error($con));
+  $sql="SELECT DName FROM masterdrinks WHERE DKey >= '$q' AND DKey <= '$g'";
 
-  // SQL Server Extension Sample Code:
-  $connectionInfo = array(
-  "UID" => "namdo@coppermoon",
-   "pwd" => "Qu3nm4tr0i",
-   "Database" => "thecoppermoon",
-   "LoginTimeout" => 30, "Encrypt" => 1,
-   "TrustServerCertificate" => 0
- );
-
-  $serverName = "tcp:coppermoon.database.windows.net,1433";
-
-  $conn = sqlsrv_connect($serverName, $connectionInfo);
-  //$con = mysqli_connect("localhost","root","","thecoppermoon") or die("Error".mysqli_error($con));
-  $sql="SELECT DName FROM masterdrinks WHERE DKey >= 0 AND DKey <= 30";
-
-  $result = sqlsrv_query($conn,$sql) or die("Error in Selecting".mysqli_error($conn));
+  $result = mysqli_query($con,$sql) or die("Error in Selecting".mysqli_error($con));
 
   $emparray = array();
 
@@ -37,7 +16,7 @@
       echo "<a href=# onclick=getData(this.innerHTML);>" .$row['DName']. "</a>";
     //   echo "<br>";
   }
-  mysqli_close($conn);
+  mysqli_close($con);
   ?>
   </body>
 </html>
